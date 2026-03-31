5c411aae685156ef534a8441893ca993f846107a
onbehalfofhim <lobovamarina13@gmail.com>
2026-03-31 09:10:50 +0300

    correct reciever name

--- internal/repository/postgres/postgres_storage.go
+++ internal/repository/postgres/postgres_storage.go
@@ -17,26 +17,26 @@ func (p *PostgresStorage) Ping(ctx context.Context) error {
 	return p.db.PingContext(ctx)
 }
 
-func (s *PostgresStorage) UpdateGauge(name string, value float64) error {
+func (p *PostgresStorage) UpdateGauge(name string, value float64) error {
 	return nil
 }
 
-func (s *PostgresStorage) GetGauge(name string) (float64, error) {
+func (p *PostgresStorage) GetGauge(name string) (float64, error) {
 	return 0, nil
 }
 
-func (s *PostgresStorage) GetListGauges() map[string]float64 {
+func (p *PostgresStorage) GetListGauges() map[string]float64 {
 	return map[string]float64{}
 }
 
-func (s *PostgresStorage) UpdateCounter(name string, value int64) error {
+func (p *PostgresStorage) UpdateCounter(name string, value int64) error {
 	return nil
 }
 
-func (s *PostgresStorage) GetCounter(name string) (int64, error) {
+func (p *PostgresStorage) GetCounter(name string) (int64, error) {
 	return 0, nil
 }
 
-func (s *PostgresStorage) GetListCounters() map[string]int64 {
+func (p *PostgresStorage) GetListCounters() map[string]int64 {
 	return map[string]int64{}
 }
