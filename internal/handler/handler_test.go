6af7835a6a3fa7fece5914917b7231ff834c678b
onbehalfofhim <lobovamarina13@gmail.com>
2026-04-02 01:30:21 +0300

    correct tests

--- internal/handler/handler_test.go
+++ internal/handler/handler_test.go
@@ -106,7 +106,6 @@ func Test_GetMetricHandler(t *testing.T) {
 
 	service.UpdateMetric("gauge", "metric1", "8.7")
 	service.UpdateMetric("counter", "metric2", "-9")
-	service.UpdateMetric("counter", "METric", "3")
 
 	r := chi.NewRouter()
 	r.Get("/value/{type}/{name}", h.GetMetricHandler())
@@ -133,12 +132,6 @@ func Test_GetMetricHandler(t *testing.T) {
 			request:      "/value/counter/metric2",
 			expectedCode: http.StatusOK,
 			want:         "-9",
-		},
-		{
-			name:         "check metric name - toLowerCase",
-			request:      "/value/counter/metric",
-			expectedCode: http.StatusOK,
-			want:         "3",
 		},
 		{
 			name:         "metric not exists",
