0e3b9e0676baa7a37cb5523451e561768deede5e
onbehalfofhim <lobovamarina13@gmail.com>
2026-06-30 22:28:35 +0300

    correct after review

--- internal/buildinfo/buildinfo.go
+++ internal/buildinfo/buildinfo.go
@@ -2,7 +2,7 @@ package buildinfo
 
 import "log"
 
-// Глоальные переменные заполняются во время сборки с помощью параметра -ldflags.
+// Глобальные переменные заполняются во время сборки с помощью параметра -ldflags.
 // Пример: go run -ldflags "-X 'github.com/onbehalfofhim/metric-alert/internal/buildinfo.Version=1.0.0'" ./cmd/server/main.go
 // Значения по умолчанию пусты; при выводе на экран они будут отображаться как "N/A".
 var (
@@ -13,22 +13,19 @@ var (
 
 // Вывод информации о сборке в стандартный поток вывода (через пакет log).
 func Print() {
-	version := Version
-	if version == "" {
-		version = "N/A"
-	}
-
-	date := Date
-	if date == "" {
-		date = "N/A"
-	}
-
-	commit := Commit
-	if commit == "" {
-		commit = "N/A"
-	}
+	version := isEmpty(Version)
+	date := isEmpty(Date)
+	commit := isEmpty(Commit)
 
 	log.Printf("Build version: %s", version)
 	log.Printf("Build date: %s", date)
 	log.Printf("Build commit: %s", commit)
+}
+
+func isEmpty(info string) string {
+	if info == "" {
+		return "N/A"
+	}
+
+	return info
 }
